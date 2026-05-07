46797dfb231ce7aed3ed4559b5541e8be5dbae94
TeiorOBardo <hpaeso@gmail.com>
2026-05-06 21:51:15 -0300

    Tentativa de revisão do sistema de pulo

--- CancoesDoHavai/Assets/Scripts/Player/Movement.cs
+++ CancoesDoHavai/Assets/Scripts/Player/Movement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class Movement : MonoBehaviour
 {
@@ -11,13 +12,18 @@ public class Movement : MonoBehaviour
     [SerializeField] LayerMask groundLayer;
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 5000f;
+    [SerializeField] float idealJumpCooldown = 0.3f;
     [SerializeField] int maxJumps = 3;
     int jumpsLeft;
+    float timeSinceJump;
     Vector2 moveDir;
 
     [SerializeField] Rigidbody2D rb;
 
     private void Awake()
+    {
+    }
+    void Start()
     {
         inputActions = PlayerInputHolder.instance.playerInput;
         jump = inputActions.FindAction("Jump");
@@ -26,26 +32,38 @@ public class Movement : MonoBehaviour
         jump.performed += Jump;
         move.performed += OnMove;
         move.canceled += OnMove;
-    }
-    void Start()
-    {
         jumpsLeft = maxJumps;
     }
 
     private void Update()
-    {
+    {   
         if(Physics2D.Raycast(transform.position, -Vector2.up,1f,groundLayer) && jumpsLeft!=maxJumps)
         {
             jumpsLeft = maxJumps;
+        }
+        else
+        {
+            timeSinceJump += Time.deltaTime;
         }
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveDir.x, rb.linearVelocityY);
-}
+    }
 
-private void OnMove(InputAction.CallbackContext value)
+
+    /*float GetFinalJumpForce()
+    {
+        
+        float finalJumpForce;
+        float precision = Mathf.Clamp(1 - 2*Mathf.Abs(timeSinceJump - idealJumpCooldown), 0.33f, 1);
+
+        finalJumpForce = jumpForce*precision;
+
+        return finalJumpForce * jumpsLeft;
+    }*/
+    private void OnMove(InputAction.CallbackContext value)
     {
        moveDir = value.ReadValue<Vector2>() * speed;
     }
@@ -53,8 +71,10 @@ private void OnMove(InputAction.CallbackContext value)
     {
         if(jumpsLeft > 0)
         {
-            rb.AddForce(Vector2.up * jumpForce*jumpsLeft, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce * jumpsLeft, ForceMode2D.Impulse);
+            //print(GetFinalJumpForce() + " " + timeSinceJump);
             jumpsLeft--;
+            timeSinceJump = 0;
         }
     }
 }
